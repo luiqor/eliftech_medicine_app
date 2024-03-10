deac93f99efa85cb0fcd3b2f21f9e8e34880d307
luiqor <vohnenna@gmail.com>
2024-03-10 16:09:17 +0200

    images of products wrapped in div

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,15 +7,17 @@ function App() {
   return (
       <div className='d-flex flex-column vh-100'>
       <header>
-        <Navbar bg='dark' variant='dark' expand='lg'>
+        <Navbar bg='dark' variant='dark' className='mb-5'>
           <Container>
             <Navbar.Brand>Drug Delivery</Navbar.Brand>
           </Container>
-          <Nav>
-            <a href="/cart">Cart</a>
+          <Nav className="m-1" variant='dark'>
+            <Nav.Link href="/cart">Cart</Nav.Link>
           </Nav>
         </Navbar>
-        Medicine Delivery app
+        <div className='text-center'>
+          <h1> Medicine Delivery app</h1>
+        </div>
       </header>
       
       <main>
@@ -23,7 +25,9 @@ function App() {
         <Row>
         {sampleProducts.map((product) => 
         (<Col key={product.id} sm={6} md={3} lg={2}>
-          <img src={product.image} alt={product.id} className="product-image"></img>
+          <div className="product-image-container">
+            <img src={product.image} alt={product.id} className="product-image"></img>
+          </div>
           <h2>{product.name}</h2>
           <p>{product.price}₴</p>
         </Col>))}
