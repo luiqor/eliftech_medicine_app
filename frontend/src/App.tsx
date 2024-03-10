c2ef004c59562960458b2a82a2a38d5eeee6fc53
luiqor <vohnenna@gmail.com>
2024-03-10 16:08:32 +0200

    fontend stores page creayed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,38 @@
-import './App.css'
+import './index.css'
+import { sampleProducts } from './data'
+import { Navbar, Container, Nav, Row, Col} from 'react-bootstrap';
 
 function App() {
 
   return (
-      <div>
+      <div className='d-flex flex-column vh-100'>
       <header>
+        <Navbar bg='dark' variant='dark' expand='lg'>
+          <Container>
+            <Navbar.Brand>Drug Delivery</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart">Cart</a>
+          </Nav>
+        </Navbar>
         Medicine Delivery app
       </header>
       
       <main>
+        <Container className='mt-4'>
+        <Row>
+        {sampleProducts.map((product) => 
+        (<Col key={product.id} sm={6} md={3} lg={2}>
+          <img src={product.image} alt={product.id} className="product-image"></img>
+          <h2>{product.name}</h2>
+          <p>{product.price}₴</p>
+        </Col>))}
+        </Row>
+        </Container>
       </main>
 
       <footer>
-        All rights reserved
+        <div className='text-center'>All rights reserved</div>
       </footer>
     </div>
   )
---
+++ frontend/src/types/Product.ts
@@ -0,0 +1,8 @@
+export type Product = {
+    name: string
+    id:string
+    price: number
+    image: string
+    sellerShop: Array<string>
+    countInStock: number
+}
