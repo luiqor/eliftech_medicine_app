c338d4d48e358e1f9a3651e703423beed4366445
luiqor <vohnenna@gmail.com>
2024-03-10 14:33:02 +0200

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+      <header>
+        Medicine Delivery app
+      </header>
+      
+      <main>
+      </main>
+
+      <footer>
+        All rights reserved
+      </footer>
+    </div>
+  )
+}
+
+export default App
